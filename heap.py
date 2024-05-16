f47bd98fcf1392ddf0f18ee0f0e0a7d8cd5692f3
Artūrs Jegurs <144435647+artuursjegurs@users.noreply.github.com>
2024-05-16 13:57:27 +0000

    16.05.2024

--- heap.py
+++ heap.py
@@ -57,15 +57,12 @@ class MaxHeap:
 
 
 def find_kth_smallest(nums, k):
-    ###################################
-    ###                             ###
-    ###                             ###
-    ###                             ###
-    ###################################
-
-
-
-
+    max=MaxHeap()
+    for num in nums:
+        max.insert(num)
+        if len(max.heap)>k:
+            max.remove()
+    return max.heap[0]
 # Test cases
 nums = [[3,2,1,5,6,4], [6,5,4,3,2,1], [1,2,3,4,5,6], [3,2,3,1,2,4,5,5,6]]
 ks = [2, 3, 4, 7]
